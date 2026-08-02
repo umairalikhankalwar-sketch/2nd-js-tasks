78b9af9b96655c8356f4aa2de8ba6cea5059d0b5
umairalikhankalwar-sketch <umair.ali.khan.kalwar@gmail.com>
2026-08-02 13:16:23 +0500

    Task 3 done

---
+++ Multilples-3.js
@@ -0,0 +1,16 @@
+let i = 1;
+
+while (i <= 30  ) {
+
+    if (i % 3 === 0 ) {
+        console.log(i + ":= is Multiple of 3");
+    }
+
+    
+
+    else {
+        console.log(i + ":= is Not Multiple of 3");
+    }
+i++;
+
+}
