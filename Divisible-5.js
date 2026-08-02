3939f5d33dad628f3fd697d576219e516af0a89a
umairalikhankalwar-sketch <umair.ali.khan.kalwar@gmail.com>
2026-08-02 13:28:15 +0500

    Task 5 done

---
+++ Divisible-5.js
@@ -0,0 +1,13 @@
+let i = 1; 
+while (i <= 20 ) {
+    if (i % 5 === 0)
+    {
+        console.log(i + ":= is Divisible by 5")
+    }
+
+    else {
+        console.log(i + ":= is Not Divisible by 5")
+    }
+
+    i++;
+}
