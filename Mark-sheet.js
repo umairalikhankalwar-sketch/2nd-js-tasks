72b4cbed0a09f8f47adf16ec7a0b7285e8707649
umairalikhankalwar-sketch <umair.ali.khan.kalwar@gmail.com>
2026-08-02 15:41:27 +0500

    Task 9 done

---
+++ Mark-sheet.js
@@ -0,0 +1,19 @@
+let marks = 40;
+
+while (marks <= 100) {
+    if (marks >= 50 ) {
+
+
+console.log("pass");
+
+    }
+
+      else  {
+
+
+console.log("Fail");
+
+    }
+
+marks +=10;
+}
---
+++ even-odd-table.js
@@ -0,0 +1,14 @@
+let i =1;
+
+while (i <+ 10 ) {
+    let result =2 * i ;
+
+    if (result  % 2 === 0 ) {
+        alert(result + "Even");
+    }
+
+    else {
+          alert(result + "Odd");
+    }
+    i++;
+}
