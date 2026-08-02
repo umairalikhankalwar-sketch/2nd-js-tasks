c3c6369d1b4090d14c6b00ef237743f2f5f1bdc6
umairalikhankalwar-sketch <umair.ali.khan.kalwar@gmail.com>
2026-08-02 13:03:28 +0500

    Task 2 done (Integers)

---
+++ integers.js
@@ -0,0 +1,18 @@
+let i = -5;
+ 
+while (i <= 5 ){
+    if (i < 0 ){
+        console.log(i + ":= is Negative")
+    }
+
+else  if  (i > 0 ){
+        console.log(i + ":= is Positive")
+    }
+
+    else {
+        console.log(i + ":= is Zero")
+    }
+   i++; 
+
+
+}
