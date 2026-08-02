8147920493d6791af1852c22098d5058e2d261ab
umairalikhankalwar-sketch <umair.ali.khan.kalwar@gmail.com>
2026-08-02 12:55:08 +0500

    Task 1 done (Even-Odd by loop)

---
+++ Even-odd-using-loop.js
@@ -0,0 +1,14 @@
+let i = 1;
+while (i <= 10 ) {
+
+    if (i % 2 === 0) {
+        console.log( i +   ":= is Even ")
+    }
+
+    else {
+        console.log(i +   ":= is Odd " )
+    }
+
+    i++;
+
+}
