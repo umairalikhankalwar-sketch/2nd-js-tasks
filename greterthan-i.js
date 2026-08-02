f30c0d1fffa9df4790be2fd5ee17e07841619963
umairalikhankalwar-sketch <umair.ali.khan.kalwar@gmail.com>
2026-08-02 13:22:45 +0500

    Task 4 done

---
+++ greterthan-i.js
@@ -0,0 +1,21 @@
+let i = 1; 
+
+while (i <= 10 ) {
+
+    if ( i < 5) {
+
+        console.log(i + "v:= is Less than 5")
+    }
+
+    else if ( i > 5) {
+
+        console.log(i + "v:= is Greater than 5")
+    }
+
+   else if (i = 5) {
+
+        console.log(i + "v:= is Equal to 5")
+    }
+
+    i++;
+}
